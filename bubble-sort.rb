50aadc924dda67c5d289d214d72075beaa69374a
cluricaune <dunn.jake90@gmail.com>
2021-12-25 15:19:07 -0500

    .

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,17 +1,18 @@
-def bubble_sort(array)
+def bubble_sort array
+  single_comparison array
+  p array
+end
+
+def single_comparison array
   array.each_with_index do |v, i|
     unless i == (array.length - 1)
       if v > array[i+1]
         array.insert((i+1), array.delete_at(i))
-        bubble_sort(array)
+        single_comparison array
       end
     end
   end
 end
 
-def single_comparison(array)
-
-end
-
 array = [4,3,78,2,0,2,4,10,90,34,33,45,45,12,2,0,100,170,17]
-p bubble_sort(array)
+bubble_sort(array)
