9c9690b63fa9bf004df28ff24e4a73b44d803986
cluricaune <dunn.jake90@gmail.com>
2021-12-25 15:12:45 -0500

    .

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,10 +3,15 @@ def bubble_sort(array)
     unless i == (array.length - 1)
       if v > array[i+1]
         array.insert((i+1), array.delete_at(i))
+        bubble_sort(array)
       end
     end
   end
 end
 
-array = [4,3,78,2,0,2]
+def single_comparison(array)
+
+end
+
+array = [4,3,78,2,0,2,4,10,90,34,33,45,45,12,2,0,100,170,17]
 p bubble_sort(array)
